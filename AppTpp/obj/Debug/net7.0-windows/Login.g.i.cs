e2e271cc020bd227c33f1c2de740ca6038796802
Akemi <eka.budhiprasetya@gmail.com>
2023-12-10 15:50:00 +0800

    refactor: mengganti warna logo

--- AppTpp/obj/Debug/net7.0-windows/Login.g.i.cs
+++ AppTpp/obj/Debug/net7.0-windows/Login.g.i.cs
@@ -53,7 +53,7 @@ namespace AppTpp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AppTpp;V1.0.0.0;component/login.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AppTpp;component/login.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Login.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
